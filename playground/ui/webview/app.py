561e06ba742c586d388d77a7952b40282c2dc839
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2017-05-22 04:20:23 -0600

    render html

--- playground/ui/webview/app.py
+++ playground/ui/webview/app.py
@@ -1,50 +1,46 @@
 import sys
 import threading
 
-from flask import Flask
+from flask import Flask, render_template
 
-
-# from PyQt5.QtWidgets import *
-# from PyQt5.QtWebKit import *
-# from PyQt5.QtWebKitWidgets import *
 from PyQt5.QtCore import QUrl, QT_VERSION_STR
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication
+
+QV = [int(x) for x in QT_VERSION_STR.split('.')]
+
+if QV[0] == 5 and QV[1] >= 6:
+    print("Using QtWebEngine")
+    from PyQt5.QtWebEngineWidgets import QWebEngineView
+else:
+    print("Using QtWebKit")
+    from PyQt5 import QtWebKitWidgets
 
 
 
 class FlaskServerThread(threading.Thread):
 
     def run(self):
-        print("RUN!")
-        app.run()
+        print("Start Flask Server!")
+        flask_app.run()
 
-app = Flask(__name__)
-@app.route("/")
+flask_app = Flask(__name__)
+@flask_app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    
     fserver = FlaskServerThread()
     fserver.setDaemon(True)
     fserver.start()
-    
+
     qapp = QApplication(sys.argv)
-    
-    qv = [int(x) for x in QT_VERSION_STR.split('.')]
+
     print("QT Ver: %s" % QT_VERSION_STR)
-    if qv[0] == 5 and qv[1] >= 6:
-        from PyQt5.QtWebEngineWidgets import QWebEngineView
+    if QV[0] == 5 and QV[1] >= 6:
         view = QWebEngineView()
-        view.load(QUrl("http://localhost:5000"))
-        view.show()
     else:
-        from PyQt5 import QtWebKit
-        from PyQt5 import QtWebKitWidgets
         view = QtWebKitWidgets.QWebView()
-        view.load(QUrl("http://localhost:5000"))
-        view.show()
 
-        print("oy")
-
+    view.load(QUrl("http://localhost:5000"))
+    view.show()
     sys.exit(qapp.exec_())
