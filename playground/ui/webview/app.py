dec3a281f62b7d727baf3497888d7b3d91815aee
Chih-Hsuan Huang <chhuang215@gmail.com>
2017-05-22 04:08:44 -0600

    webview support for qt5.3.2

--- playground/ui/webview/app.py
+++ playground/ui/webview/app.py
@@ -9,7 +9,7 @@ from flask import Flask
 # from PyQt5.QtWebKitWidgets import *
 from PyQt5.QtCore import QUrl, QT_VERSION_STR
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+
 
 
 class FlaskServerThread(threading.Thread):
@@ -34,8 +34,17 @@ if __name__ == "__main__":
     qv = [int(x) for x in QT_VERSION_STR.split('.')]
     print("QT Ver: %s" % QT_VERSION_STR)
     if qv[0] == 5 and qv[1] >= 6:
+        from PyQt5.QtWebEngineWidgets import QWebEngineView
         view = QWebEngineView()
         view.load(QUrl("http://localhost:5000"))
         view.show()
+    else:
+        from PyQt5 import QtWebKit
+        from PyQt5 import QtWebKitWidgets
+        view = QtWebKitWidgets.QWebView()
+        view.load(QUrl("http://localhost:5000"))
+        view.show()
+
+        print("oy")
 
     sys.exit(qapp.exec_())
