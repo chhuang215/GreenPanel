2e726ca996a7978e0a6d7afe0502ea0b756b5fa9
Chih-Hsuan Huang <chhuang215@gmail.com>
2017-05-31 00:08:30 -0600

    failed attempt on displaying water status

--- src/water.py
+++ src/water.py
@@ -1,6 +1,6 @@
 
 import RPi.GPIO as GPIO
-import controller.UIController as UI
+import controller
 
 class WaterSensor:
     def __init__(self, gpio_pin):
@@ -8,7 +8,7 @@ class WaterSensor:
 
     def water_level_detect(self, pin):
         print("Water is good: ", str(GPIO.input(self.pin) == GPIO.LOW))
-        UI.change_water_status(GPIO.input(self.pin) == GPIO.LOW)
+        controller.UIController.get_ui().change_water_status(GPIO.input(self.pin) == GPIO.LOW)
 
     def has_enough_water(self):
         return GPIO.input(self.pin) == GPIO.LOW
