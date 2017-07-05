dca7ab5e1cbc703d200742aad863d5631e94cce3
Chih-Hsuan Huang <chhuang215@gmail.com>
2017-07-05 16:46:05 -0600

    mainview.py: added navigation stack for back button, now navigation is functional

--- src/ui/mainview.py
+++ src/ui/mainview.py
@@ -37,7 +37,7 @@ class MainWindow(QQuickView):
         hwc = controller.HardwareController
 #        self.setSource(QUrl('MainView.qml'))
         self.setSource(QUrl.fromLocalFile('ui/MainView.qml'))
-
+        self.__nav_stack = []
         # Get root
         self.root = self.rootObject()
 
@@ -45,8 +45,7 @@ class MainWindow(QQuickView):
         self.panel_home = self.root.findChild(QQuickItem, "panelHome")
         self.panel_light = self.root.findChild(QQuickItem, "panelLight")
         self.panel_setting = self.root.findChild(QQuickItem, "panelSetting")
-        self.panel_light.setVisible(False)
-        
+
         # Get Home Panel's child elements
         self.text_temp = self.panel_home.findChild(QQuickItem, "txtTemp")
         self.light_switch = self.panel_light.findChild(QQuickItem, "swtLight")
@@ -54,7 +53,7 @@ class MainWindow(QQuickView):
 
         # When light button is clicked, nav to light panel
         self.btn_light = self.root.findChild(QQuickItem, "btnLight")
-        self.btn_light.clicked.connect(lambda: self.__panel_nav(self.panel_light)) 
+        self.btn_light.clicked.connect(lambda: self.__panel_nav(self.panel_light))
 
         # When settings button is clicked, nav to settings panel
         self.btn_setting = self.root.findChild(QQuickItem, "btnSetting")
@@ -62,8 +61,8 @@ class MainWindow(QQuickView):
 
         # When confirm button is clicked in settings, nav back to main panel
         self.btn_setting_confirm = self.root.findChild(QQuickItem, "btnConfirm")
-        self.btn_setting_confirm.clicked.connect(lambda: self.__panel_nav(self.panel_home))
         self.btn_setting_confirm.clicked.connect(self.settings_confirm)
+        self.btn_setting_confirm.clicked.connect(self.__panel_nav_back)
 
         # (Quit the app, for testing purpose)
         self.btn_quit = self.root.findChild(QQuickItem, "btnQuit")
@@ -83,12 +82,29 @@ class MainWindow(QQuickView):
         self.water_update_timer.timeout.connect(self.display_current_water_status)
         self.water_update_timer.start()
 
-        self.current_visible_panel = self.panel_home
+        self.__panel_nav(self.panel_home)
 
     def __panel_nav(self, panel):
-        self.current_visible_panel.setVisible(False)
+        if self.__nav_stack:
+            self.__nav_stack[-1].setVisible(False)
+        #self.current_visible_panel.setVisible(False)
+        try:
+            self.btn_back = panel.findChild(QQuickItem, "btnBack")
+            self.btn_back.clicked.connect(self.__panel_nav_back)
+        except AttributeError:
+            print("no back button")
+
+        self.__nav_stack.append(panel)
         panel.setVisible(True)
-        self.current_visible_panel = panel
+        # self.current_visible_panel = panel
+
+    def __panel_nav_back(self):
+
+        self.btn_back.disconnect()
+        panel = self.__nav_stack.pop()
+        panel.setVisible(False)
+
+        self.__nav_stack[-1].setVisible(True)
 
     def display_current_water_status(self):
         hwc = controller.HardwareController
