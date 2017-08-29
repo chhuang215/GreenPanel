8e8d6e3cf487b4a7e746f571cc86b7875453938a
Chih-Hsuan Huang <chhuang215@gmail.com>
2017-08-29 16:31:07 -0600

    initialize UI from main function only. Will raise exception if UI not init while trying to access it

--- src/__main__.py
+++ src/__main__.py
@@ -9,7 +9,7 @@ from PyQt5.QtGui import QFont, QGuiApplication
 import db
 
 from controller import GPIOController, UIController
-
+import ui
 import water
 import temperature
 import pump
@@ -72,8 +72,12 @@ def main():
 
         slots.syncdb()
 
-        ui_view = UIController.get_ui()
-        ui_view.root.show()
+
+
+        # Instantiate the UI
+        UIController.MAIN_UI = ui.MainWindow()
+        print("UI inited")
+        UIController.MAIN_UI.root.show()
         # ui_view.root.showFullScreen()
 
         # Start threads and timers
